8b0a325ec2ed3a0d6fd1664483282f7864d28d5d
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-26 15:46:51 +0300

    Category Service ve ek dto yazımı

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMongoCollection<Category> _collection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMongoCollection<Category> collection, IMapper mapper)
+        {
+            _collection = collection;
+            _mapper = mapper;
+        }
+
+        public Task CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategory(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategory(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catalog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategory();
+        Task CreateCategory(CreateCategoryDto createCategoryDto);
+        Task UpdateCategory(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategory(string id);
+        Task<GetByIdCategoryDto> GetByIdCategory(string id);
+    }
+}
