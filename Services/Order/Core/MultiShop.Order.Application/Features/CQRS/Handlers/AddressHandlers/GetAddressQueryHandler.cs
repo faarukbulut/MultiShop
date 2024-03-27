6747397358a5a074baee4fd0f5ac30ce7417c900
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-28 00:02:20 +0300

    Address Remove Update Handler Metotları

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
@@ -1,5 +1,4 @@
-﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
-using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
 using MultiShop.Order.Application.Interfaces;
 using MultiShop.Order.Domain.Entities;
 
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class UpdateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public UpdateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAddressCommand command)
+        {
+            var values = await _repository.GetById(command.AddressID);
+            
+            values.Detail = command.Detail;
+            values.District = command.District;
+            values.City = command.City;
+            values.UserID = command.UserID;
+
+            await _repository.Update(values);
+        }
+
+    }
+}
