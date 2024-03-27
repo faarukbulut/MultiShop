0c517dbcf868b86c7f97d7e477da58b42a907ea9
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-27 23:46:44 +0300

    Commandlar ve Create Address Command Handler

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands
+{
+    internal class RemoveAddressCommand
+    {
+        public int Id { get; set; }
+
+        public RemoveAddressCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
