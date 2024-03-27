0770ac9397d86e3b38308e5e51132d21f25eedea
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-27 23:42:41 +0300

    Address Query ve Result Sınıfları

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace MultiShop.Order.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+        Task<T> GetByFilter(Expression<Func<T, bool>> filter);
+    }
+}
