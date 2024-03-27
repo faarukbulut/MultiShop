4a91884429abf445e5ef9b3f361d963857222de6
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-27 23:33:11 +0300

    Order Domain Katmanı

--- Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
+++ Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -2,5 +2,10 @@
 {
     public class Address
     {
+        public int AddressID { get; set; }
+        public string UserID { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Detail { get; set; }
     }
 }
