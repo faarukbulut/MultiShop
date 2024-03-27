f46a304ad83c3f4b89563762befdf1f3b257bb87
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-28 01:00:39 +0300

    Ordering Handler İşlemleri

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Handlers/OrderingHandlers/UpdateOrderingCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.Mediator.Handlers.OrderingHandlers
+{
+    public class UpdateOrderingCommandHandler : IRequestHandler<UpdateOrderingCommand>
+    {
+        private readonly IRepository<Ordering> _repository;
+
+        public UpdateOrderingCommandHandler(IRepository<Ordering> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderingCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetById(request.OrderingID);
+
+            values.OrderDate = request.OrderDate;
+            values.UserID = request.UserID;
+            values.TotalPrice = request.TotalPrice;
+
+            await _repository.Update(values);
+        }
+    }
+}
