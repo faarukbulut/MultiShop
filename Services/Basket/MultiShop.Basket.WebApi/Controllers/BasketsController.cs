25f3252772e216fe01a198df5275c94630002c38
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-28 19:13:19 +0300

    Basket microservisi oluşturuldu.

---
+++ Services/Basket/MultiShop.Basket.WebApi/Controllers/BasketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.Basket.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+    }
+}
