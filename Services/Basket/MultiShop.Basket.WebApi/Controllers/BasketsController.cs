9b5409741c2baf0ccf386a4dc516cda265e338a3
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-28 19:36:35 +0300

    Basket Controller

--- Services/Basket/MultiShop.Basket.WebApi/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket.WebApi/Controllers/BasketsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.WebApi.Dtos;
+using MultiShop.Basket.WebApi.LoginServices;
+using MultiShop.Basket.WebApi.Services;
 
 namespace MultiShop.Basket.WebApi.Controllers
 {
@@ -8,5 +11,36 @@ namespace MultiShop.Basket.WebApi.Controllers
     [ApiController]
     public class BasketsController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketsController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            var values = await _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserID = _loginService.GetUserId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Ürün Sepete Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet Başarıyla Temizlendi");
+        }
+
     }
 }
