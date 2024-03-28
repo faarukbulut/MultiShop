9d594d940b72625703e909a8308d23901634b3e1
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-28 18:51:06 +0300

    Cargo Operation Controller

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoOperationsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.CargoOperationDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoOperationsController : ControllerBase
+    {
+        private readonly ICargoOperationService _cargoOperationService;
+
+        public CargoOperationsController(ICargoOperationService cargoOperationService)
+        {
+            _cargoOperationService = cargoOperationService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoOperationList()
+        {
+            var values = _cargoOperationService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult CargoOperationById(int id)
+        {
+            var values = _cargoOperationService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCargoOperation(CreateCargoOperationDto createCargoOperationDto)
+        {
+            _cargoOperationService.TInsert(new CargoOperation
+            {
+                Barcode = createCargoOperationDto.Barcode,
+                Description = createCargoOperationDto.Description,
+                OperationDate = createCargoOperationDto.OperationDate,
+            });
+
+            return Ok("Oluşturma Başarılı");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveCargoOperation(int id)
+        {
+            _cargoOperationService.TDelete(id);
+            return Ok("Silme Başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCargoOperation(UpdateCargoOperationDto updateCargoOperationDto)
+        {
+            _cargoOperationService.TUpdate(new CargoOperation
+            {
+                CargoOperationID = updateCargoOperationDto.CargoOperationID,
+                Barcode = updateCargoOperationDto.Barcode,
+                Description = updateCargoOperationDto.Description,
+                OperationDate = updateCargoOperationDto.OperationDate,
+            });
+            return Ok("Düzenleme Başarılı");
+        }
+    }
+}
