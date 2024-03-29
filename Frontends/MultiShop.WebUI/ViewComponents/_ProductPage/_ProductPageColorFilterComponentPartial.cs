b36b98856e178fdb4f945341cfbbd9320ba6a7a5
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-29 21:47:44 +0300

    Ürünler Sayfası Filtreleme Alanı Component

---
+++ Frontends/MultiShop.WebUI/ViewComponents/_ProductPage/_ProductPageColorFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._ProductPage
+{
+    public class _ProductPageColorFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
