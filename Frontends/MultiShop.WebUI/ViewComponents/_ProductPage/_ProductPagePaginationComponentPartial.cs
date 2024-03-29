f254434a93bd15f4f4cf17148a8fd7476be0e78d
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-29 21:57:14 +0300

    Ürünler sayfası tamamlandı

---
+++ Frontends/MultiShop.WebUI/ViewComponents/_ProductPage/_ProductPagePaginationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._ProductPage
+{
+    public class _ProductPagePaginationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/_ProductPage/_ProductPageProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._ProductPage
+{
+    public class _ProductPageProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
