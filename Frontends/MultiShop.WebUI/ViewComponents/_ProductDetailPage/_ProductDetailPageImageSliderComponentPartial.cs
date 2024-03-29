6fcb6c118dcb2ac7a93480aa0afdb03264679ad5
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-29 22:56:45 +0300

    Ürün Detay Sayfa Tasarımı

---
+++ Frontends/MultiShop.WebUI/ViewComponents/_ProductDetailPage/_ProductDetailPageImageSliderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._ProductDetailPage
+{
+    public class _ProductDetailPageImageSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/_ProductDetailPage/_ProductDetailPageProductReviewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._ProductDetailPage
+{
+    public class _ProductDetailPageProductReviewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
