8ab962afc4f6c1c0e3ba50a9ecfcf9caa8fc80f0
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-29 23:10:51 +0300

    Sepet sayfası tasarımı

---
+++ Frontends/MultiShop.WebUI/ViewComponents/_CartPage/_CartPageProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents._CartPage
+{
+    public class _CartPageProductListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
