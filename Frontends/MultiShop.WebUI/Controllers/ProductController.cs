f36cb3ce9331159e585f3a6797e4b41ff173527f
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-31 22:10:39 +0300

    Ürüne ait bazı detayları listelemeSeçilen kategoriye göre ürünleri listeleme

--- Frontends/MultiShop.WebUI/Controllers/ProductController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductController.cs
@@ -4,13 +4,15 @@ namespace MultiShop.WebUI.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
+            ViewBag.CategoryID = id;
             return View();
         }
 
         public IActionResult Detail(string id)
         {
+            ViewBag.ProductID = id;
             return View();
         }
     }
