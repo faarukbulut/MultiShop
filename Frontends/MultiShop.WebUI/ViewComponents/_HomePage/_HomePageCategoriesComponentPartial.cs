064b75cfc5862f742e83bb74f98b0e828c9e908f
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-31 14:36:06 +0300

    Kategori resim değeri ve ui'da listelenmesi

--- Frontends/MultiShop.WebUI/ViewComponents/_HomePage/_HomePageCategoriesComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/_HomePage/_HomePageCategoriesComponentPartial.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents._HomePage
 {
     public class _HomePageCategoriesComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomePageCategoriesComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
