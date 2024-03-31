82dae72f0fad87bde27d43520466c25e69eeb09e
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-03-31 21:25:54 +0300

    about ui işlemleri

--- Frontends/MultiShop.WebUI/ViewComponents/_DefaultLayout/_DefaultLayoutFooterComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/_DefaultLayout/_DefaultLayoutFooterComponentPartial.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.AboutDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents._DefaultLayout
 {
     public class _DefaultLayoutFooterComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Abouts");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
