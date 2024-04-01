26a0b6c1e690057bb91444b0835d56df30018c16
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-04-01 13:35:54 +0300

    Register İşlemi

--- Frontends/MultiShop.WebUI/Controllers/AuthController.cs
+++ Frontends/MultiShop.WebUI/Controllers/AuthController.cs
@@ -1,17 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos.RegisterDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Login()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public AuthController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public IActionResult Login()
         {
             return View();
         }
 
+		[HttpGet]
         public IActionResult Register()
         {
             return View();
         }
-    }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Register(CreateRegisterDto createRegisterDto)
+		{
+			if(createRegisterDto.Password == createRegisterDto.ConfirmPassword)
+			{
+				var client = _httpClientFactory.CreateClient();
+				var jsonData = JsonConvert.SerializeObject(createRegisterDto);
+				StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+				var responseMessage = await client.PostAsync("https://localhost:5001/api/Registers", stringContent);
+
+				if (responseMessage.IsSuccessStatusCode)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+
+			return View();
+		}
+	}
 }
