b470dcf20005ad5d612d31e71ede2d115ec2f339
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2024-04-10 12:30:19 +0300

    Address Services

---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/AddressServices/AddressService.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.DtoLayer.OrderDtos.AddressDtos;
+
+namespace MultiShop.WebUI.Services.OrderServices.AddressServices
+{
+    public class AddressService : IAddressService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AddressService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task CreateAddress(CreateAddressDto createAddressDto)
+        {
+            await _httpClient.PostAsJsonAsync<CreateAddressDto>("addresses", createAddressDto);
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Services/OrderServices/AddressServices/IAddressService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DtoLayer.OrderDtos.AddressDtos;
+
+namespace MultiShop.WebUI.Services.OrderServices.AddressServices
+{
+    public interface IAddressService
+    {
+        //Task<List<ResultAddressDto>> GetAllAddress();
+        Task CreateAddress(CreateAddressDto createAddressDto);
+        //Task UpdateAddress(UpdateAddressDto updateAddressDto);
+        //Task DeleteAddress(string id);
+        //Task<UpdateAddressDto> GetByIdAddress(string id);
+    }
+}
